eaec6d5fa851f3ae4a139bcc75cfa5342033d731
kn3zzz <74219296+kn3zzz@users.noreply.github.com>
2021-10-28 05:07:36 +0200

    fix description

---
+++ release.config.js
@@ -0,0 +1,5 @@
+module.export = {
+    branches: "master",
+    repositoryUrl: "https://github.com/kn3zzz/my-app",
+    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
+}
