561365623f816c4d7797d59371fca5540b3c4b46
kn3zzz <74219296+kn3zzz@users.noreply.github.com>
2021-10-28 05:25:06 +0200

    fix code prettier

--- release.config.js
+++ release.config.js
@@ -1,5 +1,9 @@
 module.export = {
-    branches: "master",
-    repositoryUrl: "https://github.com/kn3zzz/my-app",
-    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
-}
+  branches: "master",
+  repositoryUrl: "https://github.com/kn3zzz/my-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+};
